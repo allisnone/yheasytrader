4cfdcca0b769cffc6240829ab04b1973b86596d9
Administrator <Administrator@USER-20150725QL>
2016-03-16 00:42:09 +0800

    update radom

--- easytrader/ranadomStock.py
+++ easytrader/ranadomStock.py
@@ -1,5 +1,5 @@
 # -*- coding:utf-8 -*-
-import random
+import random,math
 
 def get_random_price(last_price,direction):
     #direction=[-1,1,-1,1,-1,1,-1,1,-1,1,-1,1,-1,1,-1,1,-1,1,-1,1,-1,1,-1,1,-1,1,-1,1,-1,1,-1,1,-1,1,-1,1,-1,1,-1,1,-1,1,-1,1,-1,1,-1,1]
@@ -11,15 +11,36 @@ def get_random_price(last_price,direction):
     print('random_close=',random_close)
     return random_close
 
+def get_direction(count=100,pos_rate=0.5):
+    di1=int(count*pos_rate)*[1]+int(count*(1-pos_rate))*[-1]
+    return di1
 
-last_price=10.0
-count=0
-direction=[-1,1]
-while True:
-    print('count=',count)
-    random_close=get_random_price(last_price)
-    last_price=random_close
-    count=count+1
-    if count>1000:
-        break
+def get_continue_close(last_price=10.0):
+    di=get_direction(count=100, pos_rate=0.51)
+    print(di)
+    count=0
+    direction=[-1,1]
+    while True:
+        print('count=',count)
+        random_close=get_random_price(last_price,di)
+        last_price=random_close
+        count=count+1
+        if count>1000:
+            break
+    
+    return last_price
+
+j=0
+final_list=[]
+for i in range(1,100):
+    last_price=10.0
+    final_price=get_continue_close(last_price)
+    print('final_price=',final_price)
+    final_list.append(final_price)
+    if final_price>last_price:
+        j=j+1
+print('j=',j)
+if final_list:
+    print('average=',sum(final_list)/len(final_list))
+    
     
